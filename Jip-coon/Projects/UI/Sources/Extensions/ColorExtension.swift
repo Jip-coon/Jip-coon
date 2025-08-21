20ef55a98974642cabfcf6e8b0ac0c67e901db05
Gwan-Son <id147369@naver.com>
2025-08-21 12:55:23 +0900

    [Design] UIColor Hex코드로 변환하는 코드 추가 및 메인 컬러 추가

--- Jip-coon/Projects/UI/Sources/Extensions/ColorExtension.swift
+++ Jip-coon/Projects/UI/Sources/Extensions/ColorExtension.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+public extension UIColor {
+    convenience init(hexCode: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+
+    static let mainColor = UIColor(hexCode: "#FB8E49") // color like orange...?
+    static let secondaryColor = UIColor(hexCode: "#FF7747") // color like more orange
+}
