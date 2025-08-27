23867c07fd000372250483fce433f7201639f23d
Gwan-Son <id147369@naver.com>
2025-08-27 17:15:21 +0900

    [Feat] AuthService 간략하게 생성

--- Jip-coon/Projects/Core/Sources/Auth/AuthService.swift
+++ Jip-coon/Projects/Core/Sources/Auth/AuthService.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+public final class AuthService: AuthServiceProtocol {
+    public init() {}
+
+    public func signIn(email: String, password: String) async throws {
+        try await Auth.auth().signIn(withEmail: email, password: password)
+    }
+
+    public func signUp(email: String, password: String) async throws {
+        try await Auth.auth().createUser(withEmail: email, password: password)
+    }
+
+    public func signOut() throws {
+        try Auth.auth().signOut()
+    }
+
+    public var isLoggedIn: Bool {
+        return Auth.auth().currentUser != nil
+    }
+}
