ceb799c816048ad2ecf61010f084ad4c116f1d44
suri0000 <yesle2005@naver.com>
2025-08-27 21:38:41 +0900

    [Feat]#20 - SignUpLabel

--- Jip-coon/Projects/Feature/Scenes/SignUpScene/SignUpViewController.swift
+++ Jip-coon/Projects/Feature/Scenes/SignUpScene/SignUpViewController.swift
@@ -6,13 +6,37 @@
 //
 
 import UIKit
+import UI
 
-class SignUpViewController: UIViewController {
-
-    override func viewDidLoad() {
+public final class SignUpViewController: UIViewController {
+    private let signUpLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sign Up"
+        label.textColor = .mainOrange
+        label.font = .systemFont(ofSize: 48, weight: .bold)
+        return label
+    }()
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
     }
     
+    private func setUpView() {
+        view.backgroundColor = .backgroundWhite
+        
+        [signUpLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        [signUpLabel].forEach {
+            view.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            signUpLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 73),
+            signUpLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
     
-
 }
