4181545924627cd708a36a3f40ab297e79593975
suri0000 <yesle2005@naver.com>
2025-09-09 19:11:02 +0900

    [Feat]#32 - Pretendard Font

--- Jip-coon/Projects/Feature/Scenes/AddMissionScene/View/CategoryCarouselViewCell.swift
+++ Jip-coon/Projects/Feature/Scenes/AddMissionScene/View/CategoryCarouselViewCell.swift
@@ -6,7 +6,28 @@
 //
 
 import UIKit
+import UI
 
 class CategoryCarouselViewCell: UICollectionViewCell {
+    static let identifier = CategoryCarouselViewCell.self.description()
     
+    private let categoryIcon: UILabel = {
+        let label = UILabel()
+        label.font = .pretendard(ofSize: 36, weight: .regular)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        
+    }
 }
