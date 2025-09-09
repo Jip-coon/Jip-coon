fcf8b6541184cce3812a10639e069be2852895c0
suri0000 <yesle2005@naver.com>
2025-09-09 18:44:18 +0900

    [Feat]#32 - Add CollectionViewFiles

---
+++ Jip-coon/Projects/Feature/Scenes/AddMissionScene/View/CategoryCarouselView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCarouselView.swift
+//  Feature
+//
+//  Created by 예슬 on 9/9/25.
+//
+
+import UIKit
+
+class CategoryCarouselView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ Jip-coon/Projects/Feature/Scenes/AddMissionScene/View/CategoryCarouselViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCarouselViewCell.swift
+//  Feature
+//
+//  Created by 예슬 on 9/9/25.
+//
+
+import UIKit
+
+class CategoryCarouselViewCell: UICollectionViewCell {
+    
+}
