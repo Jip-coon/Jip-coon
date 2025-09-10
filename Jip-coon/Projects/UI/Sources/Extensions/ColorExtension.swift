88197011a05c149113aa08197fd5b7a802923f81
suri0000 <yesle2005@naver.com>
2025-09-10 18:42:00 +0900

    [Feat]#32 - Add Category Color

--- Jip-coon/Projects/UI/Sources/Extensions/ColorExtension.swift
+++ Jip-coon/Projects/UI/Sources/Extensions/ColorExtension.swift
@@ -20,9 +20,31 @@ public extension UIColor {
     static let textFieldStroke = UIColor(named: "textFieldStroke", in: .module, compatibleWith: nil)!
     static let textGray = UIColor(named: "textGray", in: .module, compatibleWith: nil)!
     
-    static let backgroundWhite = UIColor(named: "backgroundWhite", in: .module, compatibleWith: nil)!
-    static let textRed = UIColor(named: "textRed", in: .module, compatibleWith: nil)!
+    /// Categories
     static let green1 = UIColor(named: "green1", in: .module, compatibleWith: nil)!
     static let blue1 = UIColor(named: "blue1", in: .module, compatibleWith: nil)!
+    static let blue2 = UIColor(named: "blue2", in: .module, compatibleWith: nil)!
     static let purple1 = UIColor(named: "purple1", in: .module, compatibleWith: nil)!
+    static let red1 = UIColor(named: "red1", in: .module, compatibleWith: nil)!
+    static let yellow1 = UIColor(named: "yellow1", in: .module, compatibleWith: nil)!
+    static let brown1 = UIColor(named: "brown1", in: .module, compatibleWith: nil)!
+    
+    // 나머지
+    static let backgroundWhite = UIColor(named: "backgroundWhite", in: .module, compatibleWith: nil)!
+    static let textRed = UIColor(named: "textRed", in: .module, compatibleWith: nil)!
+    
+    static func questCategoryColor(for colorName: String) -> UIColor {
+        switch colorName {
+            case "blue1": return .blue1
+            case "blue2": return .blue2
+            case "red1": return .red1
+            case "yellow1": return .yellow1
+            case "brown1": return .brown1
+            case "orange3": return .orange3
+            case "purple1": return .purple1
+            case "green1": return .green1
+            case "textFieldStroke": return .textFieldStroke
+            default: return .systemGray
+        }
+    }
 }
