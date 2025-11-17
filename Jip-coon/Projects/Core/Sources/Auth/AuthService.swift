a8e05ca4b339c3f20d0d08a36395ab6ed450672a
Gwan-Son <id147369@naver.com>
2025-11-17 15:08:43 +0900

    [Feat] $41 - 회원탈퇴 기능 추가
    앱 재실행 시 CurrentUser를 받아오지 못하는 오류 때문에 해당 코드 작성.

--- Jip-coon/Projects/Core/Sources/Auth/AuthService.swift
+++ Jip-coon/Projects/Core/Sources/Auth/AuthService.swift
@@ -23,8 +23,20 @@ public final class AuthService: AuthServiceProtocol {
         try Auth.auth().signOut()
     }
 
-    public func deleteAccount() throws {
-        // TODO: - 회원 탈퇴 처리
+    public func deleteAccount() async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw NSError(domain: "AuthService", code: -1, userInfo: [NSLocalizedDescriptionKey: "로그인 정보가 존재하지 않습니다."])
+        }
+		
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+            user.delete { error in
+                if let error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume()
+                }
+            }
+        }
     }
 
     public var isLoggedIn: Bool {
