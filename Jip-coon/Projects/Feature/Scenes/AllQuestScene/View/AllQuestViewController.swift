2fad54d175ae258aeee42c7db06e54599749e421
suri0000 <yesle2005@naver.com>
2026-01-18 23:29:35 +0900

    [Feat]#67 - 퀘스트 타이틀

--- Jip-coon/Projects/Feature/Scenes/AllQuestScene/View/AllQuestViewController.swift
+++ Jip-coon/Projects/Feature/Scenes/AllQuestScene/View/AllQuestViewController.swift
@@ -5,13 +5,19 @@
 //  Created by 예슬 on 1/18/26.
 //
 
+import UI
 import UIKit
 
 public class AllQuestViewController: UIViewController {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        setupUI()
     }
-
+    
+    private func setupUI() {
+        view.backgroundColor = .backgroundWhite
+        navigationItem.title = "퀘스트"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
 }
