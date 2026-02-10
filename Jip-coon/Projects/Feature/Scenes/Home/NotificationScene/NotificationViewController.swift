af1a805e3a7af3168f642674d57ff52a8a97ed73
suri0000 <yesle2005@naver.com>
2026-02-11 00:44:05 +0900

    [Feat]#86 - 알림설정뷰로 이동

--- Jip-coon/Projects/Feature/Scenes/Home/NotificationScene/NotificationViewController.swift
+++ Jip-coon/Projects/Feature/Scenes/Home/NotificationScene/NotificationViewController.swift
@@ -5,14 +5,24 @@
 //  Created by 예슬 on 2/10/26.
 //
 
+import UI
 import UIKit
 
 final class NotificationViewController: UIViewController {
     
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .backgroundWhite
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemPink
+        setupUI()
+        setupNavigationRightItem()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,4 +37,43 @@ final class NotificationViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = false
     }
     
+    // MARK: - setup
+    
+    private func setupUI() {
+        view.backgroundColor = .backgroundWhite
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor
+                .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor
+                .constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor
+                .constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor
+                .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func setupNavigationRightItem() {
+        let settingButton = UIBarButtonItem(
+            image: UIImage(systemName: "gearshape"),
+            style: .plain,
+            target: self,
+            action: #selector(didTapSetting)
+        )
+        navigationItem.rightBarButtonItem = settingButton
+    }
+    
+    @objc private func didTapSetting() {
+        let notificationSettingViewModel = NotificationSettingViewModel()
+        let notificationSettingViewController = NotificationSettingViewController(
+            viewModel: notificationSettingViewModel
+        )
+        navigationController?.pushViewController(notificationSettingViewController, animated: true)
+    }
+    
 }
+
